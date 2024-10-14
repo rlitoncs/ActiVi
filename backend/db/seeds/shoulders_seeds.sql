85d0df9f3c9d5346bddbc24788679ee3d41852b2
Ann <a.m.hnatyshyn@gmail.com>
2024-10-14 18:02:14 -0300

    added description to strength exercixes

--- backend/db/seeds/shoulders_seeds.sql
+++ backend/db/seeds/shoulders_seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO Exercises (id, exercise_name, equipment,
+INSERT INTO Exercises (id, exercise_name, description, equipment,
  photo_url, video_url, muscle_group, exercise_type_id)
 VALUES 
-(16,"Smith Machine Shoulder Press", "Machine", photo_url, video_url, "Shoulders", 1);
-(17,"Dumbbell Front Raise to Lateral Raise", "Dumbbell", photo_url, video_url, "Shoulders", 1);
-(18,"Military Press", "Barbell", photo_url, video_url, "Shoulders", 1);
+(16,"Smith Machine Shoulder Press", "Sit on a flat bench under a Smith machine with the barbell at a height where your arms are nearly fully extended. Align the bar with your nose, grip it with palms forward, and lift to full extension. Lower the bar to chin level while inhaling, then press it back up using your shoulders. Repeat for the recommended reps.", "Machine", photo_url, video_url, "Shoulders", 1);
+(17,"Dumbbell Front Raise to Lateral Raise", "Stand holding a pair of dumbbells at your sides—this is your starting position. With elbows slightly bent, raise the weights straight in front of you to shoulder height without swinging. At the top, move the weights forward, keeping your arms extended, then lower them in a controlled motion. On the next rep, raise the weights to shoulder height again, but this time move them laterally to your sides before lowering them back to the starting position. Repeat as needed.", "Dumbbell", photo_url, video_url, "Shoulders", 1);
+(18,"Military Press", "Place a barbell chest-high on a squat rack and load it with your chosen weights. Grip the bar with a pronated grip, slightly wider than shoulder-width. Bend your knees slightly and rest the bar on your collarbone. Lift it onto your chest, step back, and position your feet shoulder-width apart. Raise the bar overhead by locking your arms, holding it at shoulder level slightly in front of your head—this is your starting position. Lower the bar to your collarbone as you inhale, then press it back up as you exhale. Repeat for the desired reps.", "Barbell", photo_url, video_url, "Shoulders", 1);
