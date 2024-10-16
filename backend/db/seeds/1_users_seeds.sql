d5e6a133db825e3dd1292ef7234f0bd9e3c9a093
Ann <a.m.hnatyshyn@gmail.com>
2024-10-16 10:18:54 -0300

    add user_seeds

---
+++ backend/db/seeds/1_users_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO Users (id, first_name, last_name, email, password, age,
+ weight, height, profile_pic)
+VALUES
+(1, 'Jordan', 'Stuart', 'j.stuart@gmail.com', '12345', 29, 175, 180, ''),
+(2, 'Chelsea', 'Briggs', 'c.briggs@gmail.com','12345', 32, 155, 170, '');
