e42af0152077fa7e1c34f2d83ec65d380eb67398
Ann <a.m.hnatyshyn@gmail.com>
2024-10-16 11:41:00 -0300

    add user workout seeds

--- backend/db/seeds/1_users_seeds.sql
+++ backend/db/seeds/1_users_seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO Users (id, first_name, last_name, email, password, age,
  weight, height, profile_pic)
 VALUES
-(1, 'Jordan', 'Stuart', 'j.stuart@gmail.com', '12345', 29, 175, 180, 'https://github.com/rlitoncs/ActiVi/blob/seeds/backend/public/images/jordan_stuart_profile.png'),
+(1, 'Jordan', 'Stuart', 'j.stuart@gmail.com', '12345', 29, 175, 180, 'https://github.com/rlitoncs/ActiVi/blob/seeds/backend/public/images/jordan_stuart_profile.png');
 (2, 'Chelsea', 'Briggs', 'c.briggs@gmail.com','12345', 32, 155, 170, 'https://github.com/rlitoncs/ActiVi/blob/seeds/backend/public/images/chelsea_briggs_profile.png');
