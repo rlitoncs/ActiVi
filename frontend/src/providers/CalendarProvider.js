a54bc8dcea1f2357de6729ac9132917703f25df0
rlitoncs <rj_liton@hotmail.com>
2024-10-16 23:46:31 -0400

    feat: add calendar provider

---
+++ frontend/src/providers/CalendarProvider.js
@@ -0,0 +1,24 @@
+import '../styles/Calendar.scss';
+import dayjs from 'dayjs';
+import { useState, createContext, useContext }  from 'react';
+
+export const calendarContext = createContext();
+
+export const useCalendar = () => {
+  return useContext(calendarContext)
+}
+
+const CalendarProvider = (props) => {
+  const [selectedDate, setSelectedDate] = useState(dayjs());  
+
+  const value = {selectedDate, setSelectedDate};
+
+  return (
+    
+    <calendarContext.Provider value={value}>
+      {props.children}
+    </calendarContext.Provider>
+  )
+}
+
+export default CalendarProvider;
