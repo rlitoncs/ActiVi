eb6600faab9eb94f6322639e9c9f040ffa0f1521
Ann <a.m.hnatyshyn@gmail.com>
2024-10-17 20:52:11 -0300

    edit exercise modal: custom hook and scss

--- frontend/src/components/pages/ExerciseModal.jsx
+++ frontend/src/components/pages/ExerciseModal.jsx
@@ -1,34 +1,47 @@
 import { useState, useEffect } from 'react';
 import Exercises from './Exercises';
 import WorkoutList from '../WorkoutList';
+import useExerciseModal from '../../hooks/useExerciseModal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/pages/Dashboard.scss';
 
+const ExerciseModal = ({
+  isModalOpen,
+  selectedExercise,
+  openModal,
+  closeModal,
+}) => {
+  if (!isModalOpen || !selectedExercise) return null; // Return null if modal is not open or no exercise is selected
 
-const ExerciseModal = () => {
   return (
     <div className='exercise-modal-container'>
-      <button className='exercise-modal_close-button'>x</button>
+      <button className='exercise-modal_close-button' onClick={closeModal}>
+        x
+      </button>
 
       <div className='exercise-modal_exercises'>
-        <h1>Bench Press</h1>
+        <h1>{selectedExercise.name}</h1>
       </div>
 
       <div className='exercise-modal_muscle_group'>
-        <button> Chest </button>
+        <div>
+          {selectedExercise.muscleGroup || 'Unknown Muscle Group'}
+          </div>
       </div>
 
-      <div className='exercise-modal_photo_url'>
+      <div className='exercise-modal_video_url'>
         <h1>Tutorial</h1>
-        <img>
-        src={Public.tutorials}
-        alt=''
-        </img>
+        <img
+          src={selectedExercise.video_url || '/backend/public/tutorials'}
+          alt={`${selectedExercise.exercise_name} Tutorial`}
+        />
       </div>
 
       <div className='exercise-modal_description, exercise-modal_equipment'>
         <h1>Description</h1>
+        <p>{selectedExercise.description}</p>
         <h2>Equipment</h2>
+        <p>{selectedExercise.equipment}</p>
       </div>
     </div>
   );
--- frontend/src/hooks/useExerciseModal.js
+++ frontend/src/hooks/useExerciseModal.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const useExerciseModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
