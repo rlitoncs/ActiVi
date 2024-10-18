c7710601edcbd55bb41d101cfb29107095cf7a33
Ann <a.m.hnatyshyn@gmail.com>
2024-10-18 10:24:07 -0300

    edit on styling exercise-modal

--- frontend/src/components/pages/ExerciseModal.jsx
+++ frontend/src/components/pages/ExerciseModal.jsx
@@ -11,7 +11,7 @@ const ExerciseModal = ({
   openModal,
   closeModal,
 }) => {
-  if (!isModalOpen || !selectedExercise) return null; // Return null if modal is not open or no exercise is selected
+  // if (!isModalOpen || !selectedExercise) return null; // Return null if modal is not open or no exercise is selected
 
   return (
     <div className='exercise-modal-container'>
@@ -20,28 +20,26 @@ const ExerciseModal = ({
       </button>
 
       <div className='exercise-modal_exercises'>
-        <h1>{selectedExercise.name}</h1>
+        <h1>{}</h1>
       </div>
 
       <div className='exercise-modal_muscle_group'>
         <div>
-          {selectedExercise.muscleGroup || 'Unknown Muscle Group'}
+          {}
           </div>
       </div>
 
       <div className='exercise-modal_video_url'>
         <h1>Tutorial</h1>
-        <img
-          src={selectedExercise.video_url || '/backend/public/tutorials'}
-          alt={`${selectedExercise.exercise_name} Tutorial`}
-        />
+        <img></img>
       </div>
 
-      <div className='exercise-modal_description, exercise-modal_equipment'>
+      <div className='exercise-modal_exercises'>
+        <div classname='exercise-modal_description'></div>
         <h1>Description</h1>
-        <p>{selectedExercise.description}</p>
+        <p>{}</p>
         <h2>Equipment</h2>
-        <p>{selectedExercise.equipment}</p>
+        <p>{}</p>
       </div>
     </div>
   );
