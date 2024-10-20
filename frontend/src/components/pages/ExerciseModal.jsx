843b8ddf490cb885e70f1037da867c58df1e2028
Ann <a.m.hnatyshyn@gmail.com>
2024-10-20 14:22:46 -0300

    fix close button

--- frontend/src/components/pages/ExerciseModal.jsx
+++ frontend/src/components/pages/ExerciseModal.jsx
@@ -1,30 +1,34 @@
-import { useState, useEffect } from 'react';
-import Exercises from './Exercises';
-import WorkoutList from '../WorkoutList';
 import useExerciseModal from '../../hooks/useExerciseModal';
-import Modal from '../../styles/pages/Modal.scss'
+import Modal from '../../styles/pages/Modal.scss';
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/pages/Dashboard.scss';
 
-const ExerciseModal = ({
-  isModalOpen,
-  selectedExercise,
-  closeModal,
-}) => {
-  // if (!isModalOpen || !selectedExercise) return null; // Return null if modal is not open or no exercise is selected
+const ExerciseModal = () => {
+  const { isModalOpen, selectedExercise, openModal, closeModal } =
+    useExerciseModal();
+  
+  const navigate = useNavigate();
+
+  const handleCloseModal = () => {
+    closeModal();
+    navigate('../Exercises');
+  }
 
   return (
     <div className='exercise-modal-container'>
-      <button className='exercise-modal_close-button'onClick={closeModal}>Close</button>
+      <button className='exercise-modal_close-button' onClick={handleCloseModal}>
+        Close
+      </button>
 
       <div className='exercise-modal_exercises'>
         <h1>Bench Press</h1>
       </div>
 
+      <br></br>
+
       <div className='exercise-modal_muscle_group'>
-        <div>
-          # Chest
-        </div>
+        <div># Chest </div>
       </div>
 
       <br></br>
@@ -44,7 +48,6 @@ const ExerciseModal = ({
         <div>Equipment</div>
         <p>{}</p>
       </div>
-
     </div>
   );
 };
