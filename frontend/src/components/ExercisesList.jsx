eb40a0a26ae8f6c90bff6fe5cc01e4b4fe85d86d
Henry <mbahenryemeka@gmail.com>
2024-10-22 21:54:31 -0400

    complete exercise page

--- frontend/src/components/ExercisesList.jsx
+++ frontend/src/components/ExercisesList.jsx
@@ -6,19 +6,21 @@ import '../styles/ExercisesList.scss';
 const ExerciseList = () => {
   const [exercises, setExercises] = useState([]);
   const [selectedType, setSelectedType] = useState('All');
+  const [selectedMuscle, setSelectedMuscle] = useState('All');
 
   // Function to handle dropdown change
-  const handleTypeChange = (event) => {
+  const exerciseTypeChange = (event) => {
     setSelectedType(event.target.value);
   };
 
-  
+  const musclesGroupChange = (event) => {
+    setSelectedMuscle(event.target.value);
+  }
 
   useEffect(() => {
     // Fetch data from the backend
     axios.get('/api/exercises')
       .then(response => {
-        console.log(response.data);
         setExercises(response.data);  // Set the fetched data to the state
       })
       .catch(error => {
@@ -26,39 +28,62 @@ const ExerciseList = () => {
       });
   }, []);
 
-  const filteredExercises = selectedType === 'All' 
-  ? exercises 
-  : exercises.filter(exercise => {
-      const typeId = selectedType === 'Cardio' ? 2 : 1; 
-      return exercise.exercise_type_id === typeId;
+  const filterAll = exercises.filter(exercise => {
+    const exerciseType = selectedType === 'Cardio' ? 2 : 1;
+
+    let isType = exercise.exercise_type_id === exerciseType;
+    let isMuscle = exercise.muscle_group === selectedMuscle;
+
+    if (selectedType === "All") {
+      isType = true;
+    }
+
+    if (selectedMuscle === "All") { isMuscle = true; }
+
+    const shouldKeep = isType === true && isMuscle === true;
+    return shouldKeep;
   });
 
   return (
     <div >
-       <div className="dropdown">
+      <div className="dropdown">
         <label htmlFor="exercise-type-select" className="dropdown-label">
           Exercise Type
           <span className="dropdown-arrow">&#9662;</span>
         </label>
-        <select id="exercise-type-select" onChange={handleTypeChange} className="hidden-select">
+        <select id="exercise-type-select" onChange={exerciseTypeChange} className="hidden-select">
           <option value="All">All</option>
           <option value="Cardio">Cardio</option>
           <option value="Strength">Strength</option>
         </select>
       </div>
-      <div className='exercise-list__main'>
-      {filteredExercises.map(exercise => {
-        return (<ExerciseListItem
-          key={exercise.id}
-          id={exercise.id}
-          src={exercise.photo_url}
-          alt={exercise.exercise_name}
-        />
-        )
-      })
-      }
+      {/* this is the one we need to adjust to muscle groups */}
+      <div className="dropdown">
+        <label htmlFor="exercise-type-select" className="dropdown-label">
+          Muscles Group
+          <span className="dropdown-arrow">&#9662;</span>
+        </label>
+        <select id="exercise-type-select" onChange={musclesGroupChange} className="hidden-select">
+          <option value="All">All</option>
+          <option value="Abdominals">Abdominals</option>
+          <option value="Biceps">Biceps</option>
+          <option value="Calves">Calves</option>
+          <option value="Chest">Chest</option>
+          <option value="Quadriceps">Quadriceps</option>
+          <option value="Shoulders">Shoulders</option>
+        </select>
       </div>
-      
+      <div className='exercise-list__main'>
+        {filterAll.map(exercise => {
+          return (<ExerciseListItem
+            key={exercise.id}
+            id={exercise.id}
+            src={exercise.photo_url}
+            alt={exercise.exercise_name}
+          />
+          )
+        })}        
+      </div>
     </div>
   );
 };
