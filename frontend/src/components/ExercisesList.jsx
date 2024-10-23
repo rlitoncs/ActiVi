9566271bf14a2fd93eface193f0833c92b67e599
Henry <mbahenryemeka@gmail.com>
2024-10-23 19:09:39 -0400

    complete exercise page

--- frontend/src/components/ExercisesList.jsx
+++ frontend/src/components/ExercisesList.jsx
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ExerciseListItem from './ExerciseListItem';
 import '../styles/ExercisesList.scss';
+import { FaBars, FaSearch } from "react-icons/fa";
 
 const ExerciseList = () => {
   const [exercises, setExercises] = useState([]);
   const [selectedType, setSelectedType] = useState('All');
   const [selectedMuscle, setSelectedMuscle] = useState('All');
+
+
+
+  const [filteredExercises, setFilteredExercises] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   // Function to handle dropdown change
   const exerciseTypeChange = (event) => {
@@ -17,35 +23,62 @@ const ExerciseList = () => {
     setSelectedMuscle(event.target.value);
   }
 
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   useEffect(() => {
     // Fetch data from the backend
     axios.get('/api/exercises')
       .then(response => {
         setExercises(response.data);  // Set the fetched data to the state
+        setFilteredExercises(response.data);
+        console.log(exercises);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, []);
 
-  const filterAll = exercises.filter(exercise => {
-    const exerciseType = selectedType === 'Cardio' ? 2 : 1;
+  useEffect(()=> {
+    let searchFilter = exercises.filter((exercise) => exercise.exercise_name.toLowerCase().includes(searchTerm.toLowerCase()));
+    console.log(searchFilter,'searchFilter')
 
-    let isType = exercise.exercise_type_id === exerciseType;
-    let isMuscle = exercise.muscle_group === selectedMuscle;
+    //moved logic
+    const realExercises = searchFilter.filter(exercise => {
+      const exerciseType = selectedType === 'Cardio' ? 2 : 1;
+      let isType = exercise.exercise_type_id === exerciseType;
+      let isMuscle = exercise.muscle_group === selectedMuscle;
+      if (selectedType === "All") {
+        isType = true;
+      }
+      if (selectedMuscle === "All") { isMuscle = true; }
+      const shouldKeep = isType === true && isMuscle === true;
+      return shouldKeep;
+    });
 
-    if (selectedType === "All") {
-      isType = true;
-    }
+    setFilteredExercises(realExercises);
+  }, [searchTerm, selectedMuscle, selectedType ])
 
-    if (selectedMuscle === "All") { isMuscle = true; }
-
-    const shouldKeep = isType === true && isMuscle === true;
-    return shouldKeep;
-  });
+  // logic for the Exercise Type and Muscles Group dropdown.
+    
 
   return (
     <div >
+      <form>
+        <div className="search-container">
+          <FaBars className="left-icon" />
+          <input
+            type="text"
+            placeholder="Hinted search text"
+            value={searchTerm}
+            onChange={handleSearchInputChange}
+            className="search-input"
+          />
+          <FaSearch className="right-icon" />
+        </div>
+      </form>
+      
       <div className="dropdown">
         <label htmlFor="exercise-type-select" className="dropdown-label">
           Exercise Type
@@ -57,7 +90,6 @@ const ExerciseList = () => {
           <option value="Strength">Strength</option>
         </select>
       </div>
-      {/* this is the one we need to adjust to muscle groups */}
       <div className="dropdown">
         <label htmlFor="exercise-type-select" className="dropdown-label">
           Muscles Group
@@ -74,7 +106,7 @@ const ExerciseList = () => {
         </select>
       </div>
       <div className='exercise-list__main'>
-        {filterAll.map(exercise => {
+        {filteredExercises.map(exercise => {
           return (<ExerciseListItem
             key={exercise.id}
             id={exercise.id}
@@ -82,7 +114,7 @@ const ExerciseList = () => {
             alt={exercise.exercise_name}
           />
           )
-        })}        
+        })}
       </div>
     </div>
   );
