1da9233391314f43c4ac998f9e940d29b757fc06
Ann <a.m.hnatyshyn@gmail.com>
2024-10-23 21:00:43 -0700

    add about paragaph

--- frontend/src/components/beyondMVP/About.jsx
+++ frontend/src/components/beyondMVP/About.jsx
@@ -1,9 +1,16 @@
 const About = () => {
   return(
-    <div>
-      {/* TO DO */}
-      Hello from the About Page!
+    <div className="about-container">
+
+    <h1>nice to meet you</h1>
+      <p>
+        We're Ralph, Henry and Ann of ActiVi. We believe that fitness is a journey, not a destination.
+        Our app is designed to help people of all fitness levels reach their goals, whether you're just getting started or looking to take your workouts to the next level.
+        ActiVi makes it easy to stay motivated and committed to a healthier lifestyle. We’re here to support you every step of the way, empowering you to achieve your best self with workouts tailored to your goals,
+        whether it's building strength, improving endurance, or enhancing overall wellness. Join our community and start your fitness journey with us today!
+      </p>
+
     </div>
   )
 }
-export default About;
+export default About; 
