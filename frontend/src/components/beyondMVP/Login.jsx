7c0b586f42a046cae7d78488eb604744986780b5
Henry <mbahenryemeka@gmail.com>
2024-10-25 00:25:57 -0400

    login page background image update

--- frontend/src/components/beyondMVP/Login.jsx
+++ frontend/src/components/beyondMVP/Login.jsx
@@ -11,17 +11,12 @@ const Login = () => {
       <form action="">
         <h1>Login</h1>
         <div className="input-box">
-          <input type="text" placeholder="Username" required />
-          {/*<FaUser className="icon" />*/}
+          <input type="text" placeholder="Username" required/>
+      
         </div>
         <div className="input-box">
-          <input type="password" placeholder="Password" required />
-          {/*<FaLock className="icon" />*/}
-        </div>
-        {/*<div className="remember-forgot">
-          <label><input type="checkbox" />Remember me</label>
-          <a href="#">Forgot password?</a>
-        </div>*/}
+          <input type="password" placeholder="Password" required />         
+        </div>        
         <button type="submit">Login</button>
         <div className="register-link">
           <p>Don't have an account? <a href="#">Register</a></p>
