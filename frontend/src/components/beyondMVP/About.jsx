63129f9649b6404fe74ae10e65ea97f8300b4ba8
Ann <a.m.hnatyshyn@gmail.com>
2024-10-26 15:54:49 -0700

    edit style of about page

--- frontend/src/components/beyondMVP/About.jsx
+++ frontend/src/components/beyondMVP/About.jsx
@@ -4,28 +4,46 @@ import '../../styles/navigation/Logo.scss';
 import '../../styles/navigation/MenuList.scss';
 import Sidebar from '../navigation/Sidebar';
 
+
 const About = () => {
   return (
-      
-      
-      <div className="about-container">
-        <Sidebar /> 
-        <h1>Nice to Meet You</h1>
+    <div className='about-container'>
+      <Sidebar />
 
-        <p>
-          We're Ralph, Henry, and Ann of ActiVi. We believe that fitness is a
-          journey, not a destination. Our app is designed to help people of all
-          fitness levels reach their goals, whether you're just getting started
-          or looking to take your workouts to the next level. ActiVi makes it easy to
-          stay motivated and committed to a healthier lifestyle. We’re here to
-          support you every step of the way, empowering you to achieve your best
-          self with workouts tailored to your goals, whether it's building
-          strength, improving endurance, or enhancing overall wellness. Join our
-          community and start your fitness journey with us today!
-        </p>
-    
-      
+      <div className='about-title'>
+      <p>Nice to Meet You</p>
       </div>
+
+      <div className='about-body2'>
+        <p>
+        We're Ralph, Henry, and Ann of ActiVi. We believe that fitness is a
+        journey, not a destination.
+        </p>
+      </div>
+
+      <div className='about-body3'>
+      <p>
+        Our app is designed to help people of all fitness levels reach their
+        goals, whether you're just getting started or looking to take your
+        workouts to the next level.
+      </p>
+      </div>
+
+      <div className='about-body4'>
+      <p>
+        ActiVi makes it easy to stay motivated and committed to a healthier
+        lifestyle. We’re here to support you every step of the way, empowering
+        you to achieve your best self with workouts tailored to your goals,
+        whether it's building strength, improving endurance, or enhancing
+        overall wellness.
+      </p>
+      </div>
+
+      <div className='about-body5'>
+      <p>Join our community and start your fitness journey with us today!</p>
+      </div>
+
+    </div>
   );
 };
 
