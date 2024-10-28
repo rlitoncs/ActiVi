f9e5687d00519ac0992122b3d1a9113e9a79f807
rlitoncs <rj_liton@hotmail.com>
2024-10-28 03:31:41 -0400

    fix: update to have scroll on components under home layout

--- frontend/src/components/navigation/HomeLayout.jsx
+++ frontend/src/components/navigation/HomeLayout.jsx
@@ -2,8 +2,28 @@ import '../../styles/navigation/HomeLayout.scss'
 import HomeNavigationBar from './HomeNavigationBar';
 import Footer from './Footer';
 import { Outlet } from "react-router-dom";
+import { useEffect } from 'react';
 
 const HomeLayout = () => {
+  useEffect(() => {
+    const body = document.body;
+
+    const checkOverflow = () => {
+      if (document.documentElement.scrollHeight >= window.innerHeight) {
+        body.style.overflowY = 'scroll'; // Show scrollbar when doc is greater or same height as window
+      } 
+    };
+
+    checkOverflow(); 
+    window.addEventListener('resize', checkOverflow);
+
+    //cleanup
+    return () => {
+      body.style.overflowY = '';
+      window.removeEventListener('resize', checkOverflow); 
+    };
+  }, []);
+
   return (
     <div className="home-container"> 
       <div className="home-nav-bar-container">
