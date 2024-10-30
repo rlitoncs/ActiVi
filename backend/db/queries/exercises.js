8131ea18471c4c4730d508864c3421cf3ae73b65
rlitoncs <rj_liton@hotmail.com>
2024-10-29 20:00:15 -0400

    Revert "fix: update routes for exercise modal"
    This reverts commit a7cc5bc02b0704bf51fc4a086212d51654caaad5.

--- backend/db/queries/exercises.js
+++ backend/db/queries/exercises.js
@@ -7,11 +7,4 @@ const getAllExercises = () => {
     });
 };
 
-const getExerciseIdData = (exercise_id) => {
-  return db.query('SELECT exercise_name, description, equipment, video_url, muscle_group FROM exercises WHERE id=$1', [exercise_id])
-    .then(data => {
-      return data.rows;
-    })
-}
-
-module.exports = { getAllExercises, getExerciseIdData };
+module.exports = { getAllExercises };
