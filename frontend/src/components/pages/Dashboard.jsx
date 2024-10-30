79a03efcae7e5a3d3b590088dd4555e3ce604629
rlitoncs <rj_liton@hotmail.com>
2024-10-30 13:08:59 -0400

    fix: update to use user route

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/pages/Dashboard.scss'
+import {useState, useEffect} from "react";
+import axios from "axios";
 import Calendar from '../Calendar';
 import WorkoutList from '../WorkoutList';
 import useEdit from '../../hooks/useEdit';
@@ -8,10 +10,27 @@ import EditModalWorkout from '../EditModalWorkout'
 import DeleteModalWorkout from '../DeleteModalWorkout';
 
 const Dashboard = () => {
+  const [user, setUser] = useState({});
+  console.log(user);
+
+  useEffect(() => {
+    axios.get(`/api/users/1`)
+      .then(response => {
+        setUser(response.data[0])
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, [])
+
+
+
   const { displayModal, workout, status, submit, editWorkout, closeEditWorkout, handleChange,
     handleSubmit } = useEdit();
   
   const { displayModalDelete, workoutID, statusDelete, submitDelete, deleteWorkout, closeDeleteWorkout, handleSubmitDelete } = useDelete();
+
+  
    
 
   return(
@@ -19,7 +38,7 @@ const Dashboard = () => {
       <div className="dashboard__image__container">
         <img
           className="dashboard__hero"
-          src={"https://images.unsplash.com/photo-1676312834443-06c9ec87c8f7?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+          src={"https://images.unsplash.com/photo-1649888254873-d9870ee286ee?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
         >
         </img>
 
@@ -28,6 +47,10 @@ const Dashboard = () => {
           src={"https://t3.ftcdn.net/jpg/04/97/66/28/360_F_497662812_7rGW6PMBJR9AbrKcGgN5S1luXYTjH92i.jpg"}
           >
           </img>
+          
+          <div className="user-name">
+            {user.first_name} {user.last_name}
+          </div>
 
           <div className="user-details">
             <div className="user-details-height">
@@ -47,12 +70,12 @@ const Dashboard = () => {
         </div>
       </div>
       <div className='section-header-calendar'>
-        <h1> Welcome Back, Name</h1>
+        <h1> Welcome Back, {user.first_name}</h1>
         <Calendar />
       </div>
 
       <div className='section-workouts-title'>
-        <h1>Workouts</h1>
+        <h1>My Workouts</h1>
         <hr></hr>
       </div>
 
