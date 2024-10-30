a0e04f33ba5c3075266c89ce87e27019cd6f3f55
rlitoncs <rj_liton@hotmail.com>
2024-10-29 20:50:53 -0400

    fix: update to use video

--- frontend/src/components/ExerciseModalDetails.jsx
+++ frontend/src/components/ExerciseModalDetails.jsx
@@ -2,6 +2,7 @@ import '../styles/ExerciseModalDetails.scss'
 import CloseIcon from '@mui/icons-material/Close';
 
 const ExerciseModalDetails = ({selectedExercise, closeModal}) => {
+  console.log(selectedExercise);
   return (
     <div className="exercise-details-modal">
 
@@ -15,10 +16,14 @@ const ExerciseModalDetails = ({selectedExercise, closeModal}) => {
       
       <div className="workout-list-muscle-group">{selectedExercise.muscle_group}</div>
       <div className="workout-tutorial-video">
-        <iframe 
-          src={selectedExercise.video_url}
-          allowfullscreen>
-        </iframe>
+      {selectedExercise.video_url ? (
+          <video controls width="1280" height="720" autoPlay muted>
+            <source src={`/${selectedExercise.video_url}`} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <p>Loading video...</p> // Fallback message while loading
+        )}
       </div>
 
       <div className="workout-title-description" > Description </div>
