9085f004d5c8a07c33436400464f63d484c411a0
rlitoncs <rj_liton@hotmail.com>
2024-10-31 17:29:51 -0400

    fix: update user based on login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import Login from './components/beyondMVP/Login';
-import Logout from './components/beyondMVP/Logout';
 import Layout from './components/navigation/Layout';
 import Exercises from './components/pages/Exercises';
 import Dashboard from './components/pages/Dashboard';
 import About from './components/beyondMVP/About';
-import ExerciseModal from './components/pages/ExerciseModal';
 import axios from 'axios';
 import { useState } from 'react';
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem('token')) || null);
+  const [userID, setUserID] = useState('');
 
   const handleLogin = (email, password) => {    
     console.log(email, password);
     return axios.post('/api/users/login', {email, password})
     .then(response => response.data.data)
     .then(token => {      
+      setUserID(token.id);
       localStorage.setItem('token',JSON.stringify(token))
       setCurrentUser(token);
     });
@@ -47,7 +47,7 @@ const App = () => {
       children: [
         {
           path: "/dashboard",
-          element: currentUser? <Dashboard /> : <Navigate to="/login" />
+          element: currentUser? <Dashboard userID={userID}/> : <Navigate to="/login" />
         },
         {
           path: "/exercises",
