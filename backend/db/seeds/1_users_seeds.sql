bd0d58de88af38f5bfdf5ef86f9e3ab14355d5db
rlitoncs <rj_liton@hotmail.com>
2024-10-31 17:33:56 -0400

    fix: update user seeds

--- backend/db/seeds/1_users_seeds.sql
+++ backend/db/seeds/1_users_seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO Users (id, first_name, last_name, email, password, age,
  weight, height, profile_pic)
 VALUES
-(1, 'Jordan', 'Stuart', 'j.stuart@gmail.com', '$2a$10$O4ftfxtQGMF.p.tQjds6AO931Iphelhw87Z1FNe64CRcLeOtUJljm', 29, 175, 180, 'https://t3.ftcdn.net/jpg/04/97/66/28/360_F_497662812_7rGW6PMBJR9AbrKcGgN5S1luXYTjH92i.jpg'),
-(2, 'Chelsea', 'Briggs', 'c.briggs@gmail.com','$2a$10$O4ftfxtQGMF.p.tQjds6AO931Iphelhw87Z1FNe64CRcLeOtUJljm', 32, 155, 170, 'https://images.tech.co/wp-content/uploads/2024/04/17123134/AdobeStock_657290983-1-min.jpeg');
+(1, 'Jordan', 'Stuart', 'j.stuart@gmail.com', '$2a$10$O4ftfxtQGMF.p.tQjds6AO931Iphelhw87Z1FNe64CRcLeOtUJljm', 29, 175, 180, 'https://github.com/rlitoncs/ActiVi/blob/main/backend/public/images/jordan_stuart_1.jpg?raw=true'),
+(2, 'Chelsea', 'Briggs', 'c.briggs@gmail.com','$2a$10$O4ftfxtQGMF.p.tQjds6AO931Iphelhw87Z1FNe64CRcLeOtUJljm', 32, 155, 170, 'https://github.com/rlitoncs/ActiVi/blob/main/backend/public/images/chelsea_briggs_1.jpeg?raw=true');
